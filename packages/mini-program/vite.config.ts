fd40b3adebcb563dedb978ca598a4fee2cb86fed
Zhazhazhu <fangyuan.leslie@gmail.com>
2025-09-08 16:56:01 +0800

    feat: add file path alias

--- packages/mini-program/vite.config.ts
+++ packages/mini-program/vite.config.ts
@@ -1,7 +1,11 @@
+import { resolve } from 'node:path'
 import uni from '@dcloudio/vite-plugin-uni'
 import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [uni()],
+  resolve: {
+    alias: [{ find: '~/', replacement: `${resolve(__dirname, 'src')}/` }],
+  },
 })
