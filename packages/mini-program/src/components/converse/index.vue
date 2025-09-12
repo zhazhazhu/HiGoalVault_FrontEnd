c88cd6ef2e12a6fee2c3a9298b3f19a48a505a56
Zhazhazhu <fangyuan.leslie@gmail.com>
2025-09-12 18:41:45 +0800

    fix: converse component height error

--- packages/mini-program/src/components/converse/index.vue
+++ packages/mini-program/src/components/converse/index.vue
@@ -1,6 +1,9 @@
 <script lang='ts' setup>
+import type { CSSProperties } from 'vue'
 import { useClassesName } from '@higoal/hooks'
-import { ref, watchEffect } from 'vue'
+import { ref } from 'vue'
+import { useSse } from '@/api'
+import { ClientType } from '@/api/wx'
 
 withDefaults(defineProps<{
   placeholder?: string
@@ -12,18 +15,33 @@ const model = defineModel({ type: String, default: '' })
 const cs = useClassesName('converse')
 const cursorPosition = ref(0)
 const cursorSpacing = ref(36)
-
-watchEffect(() => {
-  console.log(cursorPosition.value)
+const converseContainerStyle = ref<CSSProperties>({
+  paddingBottom: '40px',
 })
-
 function onLineChange(e) {
   cursorSpacing.value = 20 + e.height
+}
+
+function onKeyboardHeightChange(e) {
+  converseContainerStyle.value.paddingBottom = `${e.height + 20}px`
+}
+
+async function onConfirmMessage() {
+  const _model = model.value.trim()
+  model.value = ''
+  const { onChunkReceived } = useSse({
+    chatId: '123',
+    clientType: ClientType.WECHAT_MP,
+    query: _model,
+  })
+  onChunkReceived((data) => {
+    console.log(data)
+  })
 }
 </script>
 
 <template>
-  <view class="pb-40px" :class="cs.m('wrapper')">
+  <view :class="cs.m('wrapper')" :style="converseContainerStyle">
     <view :class="cs.m('container')">
       <view :class="cs.e('left')">
         <view class="i-weui-voice-outlined" :class="cs.e('icon')" />
@@ -37,15 +55,16 @@ function onLineChange(e) {
           confirm-type="send"
           placeholder-style="color: #666; line-height: 24px"
           :placeholder="placeholder"
+          :show-confirm-bar="false"
+          :adjust-position="false"
           :custom-textarea-class="cs.e('textarea')"
-          :cursor-spacing="cursorSpacing"
           :cursor="cursorPosition"
-          :selection-start="0"
-          :selection-end="cursorPosition"
           @focus="() => {
             cursorPosition = model.length
           }"
           @linechange="onLineChange"
+          @keyboardheightchange="onKeyboardHeightChange"
+          @confirm="onConfirmMessage"
         />
       </view>
 
@@ -64,7 +83,7 @@ function onLineChange(e) {
   box-shadow: 0px 2px 8px 0px rgba(0, 0, 0, 0.08);
   border-radius: 20px;
   display: flex;
-  align-items: center;
+  align-items: baseline;
 }
 .hi-converse__right {
   display: flex;
