9e0f1c128ff9a83782cf4e4966b1891dae47429d
Zhazhazhu <fangyuan.leslie@gmail.com>
2025-09-12 11:41:21 +0800

    feat: add sse api

--- packages/api/src/index.ts
+++ packages/api/src/index.ts
@@ -1,4 +1,5 @@
 import type { LoginResult } from './index.d'
+import { API } from './url'
 
 export * from './index.d'
 
@@ -25,9 +26,9 @@ export interface Options {
 
 export type Launcher = <K extends Key = 'uni'>(url: string, options?: Options[K]) => {
   get: <T>() => Promise<RequestResult<T>>
-  post: <T>(data: any) => Promise<RequestResult<T>>
-  put: <T>(data: any) => Promise<RequestResult<T>>
-  delete: <T>(data: any) => Promise<RequestResult<T>>
+  post: <T>(data?: any) => Promise<RequestResult<T>>
+  put: <T>(data?: any) => Promise<RequestResult<T>>
+  delete: <T>(data?: any) => Promise<RequestResult<T>>
 }
 export class Api {
   private launcher: Launcher
@@ -36,10 +37,22 @@ export class Api {
   }
 
   public autoLoginByPhone(data: { code: string, phoneCode: string }) {
-    return this.launcher<'uni'>('/buyer/passport/connect/miniProgram/phone/once/auto-login', { header: { ClientType: 'WECHAT_MP' } }).post<LoginResult>(data)
+    return this.launcher<'uni'>(API.AUTO_LOGIN, { header: { ClientType: 'WECHAT_MP' } }).post<LoginResult>(data)
   }
 
   public refreshToken(refreshToken: string) {
-    return this.launcher<'uni'>(`/buyer/passport/member/refresh/${refreshToken}`).get<LoginResult>()
+    return this.launcher<'uni'>(`${API.REFRESH_TOKEN}/${refreshToken}`).get<LoginResult>()
+  }
+
+  public sseConnect() {
+    return this.launcher<'uni'>(API.SSE_CONNECT).get()
+  }
+
+  public sseStop() {
+    return this.launcher<'uni'>(API.SSE_STOP).post()
+  }
+
+  public sseQuery(data: { query: string }) {
+    return this.launcher<'uni'>(API.SSE_QUERY).post(data)
   }
 }
---
+++ packages/api/src/url.ts
@@ -0,0 +1,7 @@
+export enum API {
+  AUTO_LOGIN = '/buyer/passport/connect/miniProgram/phone/once/auto-login',
+  REFRESH_TOKEN = '/buyer/passport/member/refresh',
+  SSE_CONNECT = '/buyer/chat/sse/ai-proxy/connect',
+  SSE_STOP = '/buyer/chat/sse/ai-proxy/stop-query',
+  SSE_QUERY = '/buyer/chat/sse/ai-proxy/send-query',
+}
