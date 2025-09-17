6ee8d604bf9a5f922b66a1d9b0004fc326dedc7b
Zhazhazhu <fangyuan.leslie@gmail.com>
2025-09-17 17:40:10 +0800

    style: add share svg

--- packages/mini-program/src/components/share/popup.vue
+++ packages/mini-program/src/components/share/popup.vue
@@ -8,14 +8,38 @@ const { share } = inject(chatInjectKey)!
 </script>
 
 <template>
-  <wd-popup v-model="model" position="bottom" :modal="false" root-portal>
-    <view class="p-20px h-100px">
-      <button open-type="share" :data-ids="share.ids" @click="() => model = false">
-        <view />
+  <wd-popup v-model="model" position="bottom" :modal="false" root-portal custom-style="border-radius: 20px;">
+    <view class="p-20px h-120px">
+      <button open-type="share" :data-ids="share.ids" class="share-btn" @click="() => model = false">
+        <view class="share-wechat" />
         <text>分享好友</text>
       </button>
     </view>
   </wd-popup>
 </template>
 
-<style lang='scss' scoped></style>
+<style lang='scss' scoped>
+.share-btn {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+  height: 100%;
+  background-color: #fff;
+  border-radius: 12px;
+  &::after {
+    display: none;
+  }
+  view {
+    border-radius: 100%;
+    width: 48px;
+    height: 48px;
+  }
+  text {
+    font-size: 12px;
+    color: #666666;
+    line-height: 24px;
+  }
+}
+</style>
