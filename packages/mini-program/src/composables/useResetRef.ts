eee022364a08e002618f00aba5814c4c19473387
Zhazhazhu <fangyuan.leslie@gmail.com>
2025-09-18 18:20:11 +0800

    feat: add useResetRef composable for resetting reactive references

---
+++ packages/mini-program/src/composables/useResetRef.ts
@@ -0,0 +1,14 @@
+import type { MaybeRef } from 'vue'
+import { cloneDeepWith } from 'lodash-es'
+import { isRef, ref } from 'vue'
+
+export function useResetRef<T>(value: MaybeRef<T>) {
+  const _valueDefine = cloneDeepWith(value as any)
+  const _value = isRef(value) ? value : ref(value)
+
+  function reset(value?: T) {
+    _value.value = value ? cloneDeepWith(value) : cloneDeepWith(_valueDefine)
+  }
+
+  return [_value, reset] as const
+}
