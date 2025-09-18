51e9bbb68f0d634e32fc077ad3b50c1ea006ab1e
Zhazhazhu <fangyuan.leslie@gmail.com>
2025-09-18 18:20:19 +0800

    feat: implement chat store with state management and message handling actions

---
+++ packages/mini-program/src/store/chat.ts
@@ -0,0 +1,60 @@
+import type { Chat, ChatMessageAfter, ChatMessageBefore, ChatMessageReference } from '@higoal/api'
+import { defineStore } from 'pinia'
+import { createUUID } from '@/utils'
+
+interface State {
+  chat: Chat[]
+  currentChatId: string
+  messages: ChatMessageAfter[]
+  currentTemporaryMessageId: string
+}
+
+export const useChatStore = defineStore('chat', {
+  state: (): State => ({
+    chat: [],
+    currentChatId: '',
+    messages: [],
+    currentTemporaryMessageId: '',
+  }),
+  getters: {
+    currentChat: (state) => {
+      return state.chat.find(item => item.chatId === state.currentChatId)
+    },
+    currentTemporaryMessage: (state) => {
+      return state.messages.find(item => item.id === state.currentTemporaryMessageId)
+    },
+  },
+  actions: {
+    transformMessage(message: ChatMessageBefore): ChatMessageAfter {
+      let reference: ChatMessageReference[] = []
+      if (message.reference) {
+        try {
+          reference = JSON.parse(message.reference)
+        }
+        catch (error) {
+          console.log('transformMessage error', error)
+        }
+      }
+      return {
+        ...message,
+        reference,
+      }
+    },
+    createTemporaryMessage(message?: Partial<ChatMessageAfter>): ChatMessageAfter {
+      const temp = {
+        id: createUUID(),
+        chatId: this.currentChatId || '',
+        data: [],
+        message: '',
+        query: '',
+        queryId: createUUID(),
+        response: '',
+        reference: [],
+        ...message,
+      } satisfies ChatMessageAfter
+      this.messages.push(temp)
+      this.currentTemporaryMessageId = temp.id
+      return temp
+    },
+  },
+})
