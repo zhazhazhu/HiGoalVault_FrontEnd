02a0af61124c735601823fc05517b8ace5784d1b
Zhazhazhu <fangyuan.leslie@gmail.com>
2025-09-25 13:38:47 +0800

    feat: add tag component

---
+++ packages/mini-program/src/components/tag/index.vue
@@ -0,0 +1,27 @@
+<script lang='ts' setup>
+import { useClassesName } from '@higoal/hooks'
+
+const props = defineProps<{ customClass?: string }>()
+const cs = useClassesName('tag')
+</script>
+
+<template>
+  <view :class="[cs.m('container'), props.customClass]">
+    <slot />
+  </view>
+</template>
+
+<style lang='scss' scoped>
+.hi-tag--container {
+  width: auto;
+  height: 24rpx;
+  background: #66666614;
+  border-radius: 4rpx;
+  padding: 10rpx 16rpx;
+  display: inline-flex;
+  align-items: center;
+  justify-content: center;
+  font-size: 18rpx;
+  color: var(--hi-primary-color);
+}
+</style>
