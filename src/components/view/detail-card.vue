472ed65c4990b46de1ae8fe0a99d07b695aa2093
Zhazhazhu <fangyuan.leslie@gmail.com>
2025-12-03 11:57:55 +0800

    feat: 添加分享和举报功能，优化用户交互体验

--- src/components/view/detail-card.vue
+++ src/components/view/detail-card.vue
@@ -1,6 +1,6 @@
 <script lang='ts' setup>
 import type { PublishMessageListResponse } from '@/api'
-import { watch } from 'vue'
+import { ref, watch } from 'vue'
 import { api } from '@/api'
 import { useUserStore } from '@/store'
 import { formatCommentDate } from '@/utils'
@@ -10,6 +10,7 @@ const props = defineProps<{
 }>()
 
 const userStore = useUserStore()
+const showOption = ref(false)
 
 async function checkFollowUser() {
   const res = await api.checkFollowUser(props.data!.memberId)
@@ -29,6 +30,9 @@ function onClickTag({ id }: { id: string }) {
 function gotoUser() {
   uni.navigateTo({ url: `/pages/user-package/pages/user/index?id=${props.data!.memberId}` })
 }
+function handleShare() {
+  showOption.value = false
+}
 
 watch(() => props.data, () => {
   checkFollowUser()
@@ -36,6 +40,20 @@ watch(() => props.data, () => {
 </script>
 
 <template>
+  <wd-root-portal>
+    <wd-popup v-model="showOption" position="bottom" custom-class="rounded-t-32px">
+      <view class="p-20px pb-40px">
+        <button open-type="share" class="cell-item" @click="handleShare">
+          <view class="wechat-icon icon" />
+          <text>分享</text>
+        </button>
+        <view class="cell-item warning">
+          <view class="i-ic-baseline-warning-amber icon" />
+          <text>举报内容</text>
+        </view>
+      </view>
+    </wd-popup>
+  </wd-root-portal>
   <view class="flex flex-col p-32rpx gap-20rpx">
     <view class="flex items-center justify-between text-26rpx color-#8E8E93">
       <view class="flex items-center" @click="gotoUser">
@@ -50,17 +68,22 @@ watch(() => props.data, () => {
         </view>
       </view>
 
-      <template v-if="data?.memberId !== userStore.userInfo?.id">
-        <wd-button v-if="!data?.isFollowed" size="small" :round="false" @click="onFollowUser('follow')">
-          <view class="flex items-center">
-            <view class="i-material-symbols-add text-14px" />
-            关注
-          </view>
+      <view class="flex gap-10px">
+        <template v-if="data?.memberId !== userStore.userInfo?.id">
+          <wd-button v-if="!data?.isFollowed" size="small" :round="false" @click="onFollowUser('follow')">
+            <view class="flex items-center">
+              <view class="i-material-symbols-add text-14px" />
+              关注
+            </view>
+          </wd-button>
+          <wd-button v-else size="small" type="info" :round="false" @click="onFollowUser('unfollow')">
+            已关注
+          </wd-button>
+        </template>
+        <wd-button size="small" type="info" :round="false" @click="showOption = true">
+          <view class="i-ri-more-fill text-14px" />
         </wd-button>
-        <wd-button v-else size="small" type="info" :round="false" @click="onFollowUser('unfollow')">
-          已关注
-        </wd-button>
-      </template>
+      </view>
     </view>
 
     <view class="text-15px color-#2F2E33 word-wrap font-500">
@@ -75,4 +98,27 @@ watch(() => props.data, () => {
   </view>
 </template>
 
-<style lang='css' scoped></style>
+<style lang='scss' scoped>
+.cell-item {
+  background-color: transparent;
+  display: flex;
+  align-items: center;
+  gap: 12px;
+  padding: 12px 0;
+  font-size: 14px;
+  &::after {
+    display: none;
+  }
+  &.warning {
+    color: #ff4d4f;
+  }
+  .icon {
+    font-size: 18px;
+    width: 18px;
+    height: 18px;
+  }
+  text {
+    font-size: 14px;
+  }
+}
+</style>
